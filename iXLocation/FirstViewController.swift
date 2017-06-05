4eae2516e7e6ffa506c977d1d6ec65be15de7592
Ricardo Humbert <ricardohumbert1@gmail.com>
2017-06-05 09:54:18 +0200

    first commit

---
+++ iXLocation/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  iXLocation
+//
+//  Created by Ricky Humbert on 6/5/17.
+//  Copyright © 2017 Ricky Humbert. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
